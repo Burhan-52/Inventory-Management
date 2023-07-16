8b6040045144b1c99f7bf2f1fece99b836d5ed5e
Burhan-52 <burhanrampura786@gmail.com>
2023-07-16 16:57:25 +0530

    update the url

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const connectToMongo = async () => {
         await mongoose.connect(process.env.MONGO_CONNECTION, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        })
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
